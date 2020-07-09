34f973849eb878b7d24d4696ee8168a07b6436af
Damian Mullins <89059+DamianMullins@users.noreply.github.com>
2020-07-09 08:50:12 +0100

    v2.1.0 - Removing core-js dependency. (#70)

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: ['core-js/es/array/from', path.resolve(__dirname, 'src/index.js')],
+    entry: path.resolve(__dirname, 'src/index.js'),
     output: {
         path: path.resolve(__dirname, 'dist'),
         libraryTarget: 'umd',
